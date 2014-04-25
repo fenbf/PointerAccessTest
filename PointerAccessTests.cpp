54f7353e89289e04f61a367d37f14410e3b44889
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-04-25 21:41:24 +0200

    test improvements

--- PointerAccessTests.cpp
+++ PointerAccessTests.cpp
@@ -16,41 +16,83 @@ void runTests(std::vector<std::unique_ptr<Test>> &tests, size_t count, size_t up
 	}
 }
 
-void printTests(const std::vector<std::unique_ptr<Test>> &tests)
+void printTests(const std::vector<std::unique_ptr<Test>> &tests, bool onlyShort, bool creation, bool updates)
 {
-	for (auto t = tests.begin(); t != tests.end(); ++t)
+	if (!onlyShort)
 	{
-		std::cout << std::endl << (*t)->name() << " -----" << std::endl;
-		std::cout << "memory:          " << (*t)->getMemory() << " kb" << std::endl;
-		std::cout << "creation time:   " << (*t)->getCreationTime() << " milisec" << std::endl;
-		std::cout << "updates time:    " << (*t)->getUpdateTime() << " milisec" << std::endl;
+		for (auto t = tests.begin(); t != tests.end(); ++t)
+		{
+			std::cout << std::endl << (*t)->name() << " -----" << std::endl;
+			std::cout << "memory:          " << (*t)->getMemory() << " kb" << std::endl;
+			std::cout << "creation time:   " << (*t)->getCreationTime() << " milisec" << std::endl;
+			std::cout << "updates time:    " << (*t)->getUpdateTime() << " milisec" << std::endl;
+		}
+		std::cout << std::endl;
 	}
 
-	std::cout << std::endl;
-	for (auto t = tests.cbegin(); t != tests.cend(); ++t)
-		std::cout << (*t)->getCreationTime() << ";";
-	std::cout << std::endl;
-	for (auto t = tests.cbegin(); t != tests.cend(); ++t)
-		std::cout << (*t)->getUpdateTime() << ";";
+	if (creation)
+	{
+		for (auto t = tests.cbegin(); t != tests.cend(); ++t)
+			std::cout << (*t)->getCreationTime() << ";";
+	}
+
+	if (updates)
+	{
+		for (auto t = tests.cbegin(); t != tests.cend(); ++t)
+			std::cout << (*t)->getUpdateTime() << ";";
+	}
 
 	std::cout << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-	const size_t count = argc > 1 ? atoi(argv[1]) : 20000;
-	const size_t updates = argc > 2 ? atoi(argv[2]) : 20000;
+	bool showCreation = true;
+	bool showUpdates = true;
+	if (argc > 1)
+	{
+		std::string cmd = argv[1];
+		showCreation = cmd.find('c') != std::string::npos;
+		showUpdates = cmd.find('u') != std::string::npos;
+	}
 
-	std::cout << "count: " << count << ", updates: " << updates << std::endl;
-	std::cout << "sizeof(Particle): " << sizeof(Particle) << std::endl;
+	//std::cout << "count: " << count << ", updates: " << updates << std::endl;
+	//std::cout << "sizeof(Particle): " << sizeof(Particle) << std::endl;
 
 	std::vector<std::unique_ptr<Test>> tests;
 	tests.emplace_back(new TestVectorOfObjects());
 	tests.emplace_back(new TestVectorOfPointers());
 	tests.emplace_back(new TestParticleData());
 
-	runTests(tests, count, updates);
-	printTests(tests);
+	const size_t countStart = 1000;
+	const size_t countEnd = 51000;
+	const size_t countSteps = 10;
+	const size_t countDelta = (countEnd - countStart) / countSteps;
+
+	const size_t updateStart = 1000;
+	const size_t updateEnd = 21000;
+	const size_t updateSteps = 10;
+	const size_t updateDelta = (updateEnd - updateStart) / updateSteps;
+
+	if (showCreation)
+	{
+		for (size_t count = countStart; count <= countEnd; count += countDelta)
+		{
+			std::cout << count << ";";
+			runTests(tests, count, updateStart);
+			printTests(tests, true, showCreation, showUpdates);
+		}
+	}
+
+	if (showUpdates)
+	{
+		for (size_t updates = updateStart; updates <= updateEnd; updates += updateDelta)
+		{
+			std::cout << updates << ";";
+			runTests(tests, countStart, updates);
+			printTests(tests, true, showCreation, showUpdates);
+		}
+	}
 
 	return 0;
 }
--- particles.h
+++ particles.h
@@ -21,7 +21,8 @@ public:
 		acc[0] = randF();
 		acc[1] = randF();
 		acc[2] = randF();
-		pos[0] = pos[1] = pos[2] = 0.0f;
+		acc[3] = randF();
+		pos[0] = pos[1] = pos[2] = pos[3] = 0.0f;
 		vel[0] = randF();
 		vel[1] = randF();
 		vel[2] = randF();
@@ -35,12 +36,15 @@ public:
 		vel[0] += acc[0] * dt;
 		vel[1] += acc[1] * dt;
 		vel[2] += acc[2] * dt;
+		vel[3] += acc[3] * dt;
 		pos[0] += vel[0] * dt;
 		pos[1] += vel[1] * dt;
 		pos[2] += vel[2] * dt;
+		pos[3] += vel[3] * dt;
 		col[0] = pos[0] * 0.001f;
 		col[1] = pos[1] * 0.001f;
 		col[2] = pos[2] * 0.001f;
+		col[3] = pos[3] * 0.001f;
 		rot += vel[3] * dt;
 		time -= dt;
 
@@ -53,30 +57,52 @@ class ParticleData
 {
 public:
 	const size_t _count;
-	std::unique_ptr<float[]> _data;
-	float *pos;
-	float *acc;
-	float *vel;
-	float *col;
-	float *rot;
-	float *time;
-	/*std::unique_ptr<float[]> pos;
+	//std::unique_ptr<float[]> _data;
+	//float *pos;
+	//float *acc;
+	//float *vel;
+	//float *col;
+	//float *rot;
+	//float *time;
+	std::unique_ptr<float[]> pos;
+	std::unique_ptr<float[]> acc;
 	std::unique_ptr<float[]> vel;
 	std::unique_ptr<float[]> col;
 	std::unique_ptr<float[]> rot;
-	std::unique_ptr<float[]> time;*/
+	std::unique_ptr<float[]> time;
 
 public:
 	explicit ParticleData(size_t count) :
 		_count(count),
-		_data(new float[(4 + 4 + 4 + 4 + 1 + 1) * count])
+		//_data(new float[(4 + 4 + 4 + 4 + 1 + 1) * count])
+		pos(new float[count * 4]),
+		acc(new float[count * 4]),
+		vel(new float[count * 4]),
+		col(new float[count * 4]),
+		rot(new float[count]),
+		time(new float[count])
 	{
-		pos = _data.get() + 0;
+		/*pos = _data.get() + 0;
 		acc = _data.get() + count * 4;
 		vel = _data.get() + count * (4 + 4);
 		col = _data.get() + count * (4 + 4 + 4);
 		rot = _data.get() + count * (4 + 4 + 4 + 4);
-		time = _data.get() + count * (4 + 4 + 4 + 4 +1);
+		time = _data.get() + count * (4 + 4 + 4 + 4 +1);*/
+	}
+
+	static void generate(ParticleData *p, int id)
+	{
+		p->acc[id * 4 + 0] = randF();
+		p->acc[id * 4 + 1] = randF();
+		p->acc[id * 4 + 2] = randF();
+		p->acc[id * 4 + 3] = randF();
+		p->pos[id * 4 + 0] = p->pos[id * 4 + 1] = p->pos[id * 4 + 2] = p->pos[id * 4 + 2] = 0.0f;
+		p->vel[id * 4 + 0] = randF();
+		p->vel[id * 4 + 1] = randF();
+		p->vel[id * 4 + 2] = randF();
+		p->vel[id * 4 + 3] = p->vel[id * 4 + 1] + p->vel[id * 4 + 2];
+		p->rot[id] = 0.0f;
+		p->time[id] = 2.0f + randF();
 	}
 
 	static void generate(ParticleData *p)
@@ -88,10 +114,11 @@ public:
 			p->acc[i * 4 + 0] = randF();
 			p->acc[i * 4 + 1] = randF();
 			p->acc[i * 4 + 2] = randF();
+			p->acc[i * 4 + 3] = randF();
 		}
 
 		for (size_t i = 0; i < count; ++i)
-			p->pos[i * 4 + 0] = p->pos[i * 4 + 1] = p->pos[i * 4 + 2] = 0.0f;		
+			p->pos[i * 4 + 0] = p->pos[i * 4 + 1] = p->pos[i * 4 + 2] = p->pos[i * 4 + 3] = 0.0f;
 
 		for (size_t i = 0; i < count; ++i)
 		{
@@ -115,9 +142,11 @@ public:
 			p->vel[i * 4 + 0] += p->acc[i * 4 + 0] * dt;
 			p->vel[i * 4 + 1] += p->acc[i * 4 + 1] * dt;
 			p->vel[i * 4 + 2] += p->acc[i * 4 + 2] * dt;
+			p->vel[i * 4 + 3] += p->acc[i * 4 + 3] * dt;
 			p->pos[i * 4 + 0] += p->vel[i * 4 + 0] * dt;
 			p->pos[i * 4 + 1] += p->vel[i * 4 + 1] * dt;
 			p->pos[i * 4 + 2] += p->vel[i * 4 + 2] * dt;
+			p->pos[i * 4 + 3] += p->vel[i * 4 + 3] * dt;
 		}
 
 		for (size_t i = 0; i < p->_count; ++i)
@@ -125,6 +154,7 @@ public:
 			p->col[i * 4 + 0] = p->pos[i * 4 + 0] * 0.001f;
 			p->col[i * 4 + 1] = p->pos[i * 4 + 1] * 0.001f;
 			p->col[i * 4 + 2] = p->pos[i * 4 + 2] * 0.001f;
+			p->col[i * 4 + 3] = p->pos[i * 4 + 3] * 0.001f;
 		}
 
 		for (size_t i = 0; i < p->_count; ++i)
@@ -136,18 +166,7 @@ public:
 		for (size_t i = 0; i < p->_count; ++i)
 		{
 			if (p->time[i] < 0.0f)
-			{
-				p->acc[i * 4 + 0] = randF();
-				p->acc[i * 4 + 1] = randF();
-				p->acc[i * 4 + 2] = randF();
-				p->pos[i * 4 + 0] = p->pos[i * 4 + 1] = p->pos[i * 4 + 2] = 0.0f;
-				p->vel[i * 4 + 0] = randF();
-				p->vel[i * 4 + 1] = randF();
-				p->vel[i * 4 + 2] = randF();
-				p->vel[i * 4 + 3] = p->vel[i * 4 + 1] + p->vel[i * 4 + 2];
-				p->rot[i] = 0.0f;
-				p->time[i] = 2.0f + randF();
-			}
+				generate(p, i);
 		}
 	}
 
